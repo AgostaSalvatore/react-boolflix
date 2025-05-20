3fe6ed39458b84eda7c42a0e61477b4be6f35e32
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-20 12:21:07 +0200

    feat: create initial app structure with search input and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+
+const apiKey = '1aeffb0b3520785500a2dd89ea7e745f'
 
 function App() {
+
+  const [input, setInput] = useState('');
+
 
 
   return (
@@ -12,10 +18,10 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-12 col-md-8">
-            <input type="text" placeholder="Cerca" className='form-control' />
+            <input type="text" placeholder="Cerca" className='form-control' onChange={(e) => setInput(e.target.value)} />
           </div>
           <div className="col-12 col-md-4">
-            <button className='btn btn-secondary'> Cerca </button>
+            <button className='btn btn-secondary' onClick={handleClick}> Cerca </button>
           </div>
         </div>
       </div>
