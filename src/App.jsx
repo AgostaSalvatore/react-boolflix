e33543958af7bd8703594528283c5d709c7b1e93
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-20 12:14:39 +0200

    feat: create initial BoolFlix app structure with search UI and dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
@@ -8,6 +9,16 @@ function App() {
       <header>
         <h1>BoolFlix</h1>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8">
+            <input type="text" placeholder="Cerca" className='form-control' />
+          </div>
+          <div className="col-12 col-md-4">
+            <button className='btn btn-secondary'> Cerca </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
