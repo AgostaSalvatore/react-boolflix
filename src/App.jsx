b4ac5ea5fb1ac3d93eac9b6d10beb094d23d22ba
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-20 12:56:39 +0200

    [UPD] Searchbar on header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 
-const apiKey = '1aeffb0b3520785500a2dd89ea7e745f'
 
 function App() {
 
@@ -10,7 +9,7 @@ function App() {
   const [film, setFilm] = useState([]);
 
   const handleClick = () => {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${input}`)
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1aeffb0b3520785500a2dd89ea7e745f&query=${input}`)
       .then((response) => {
         setFilm(response.data.results);
       })
@@ -23,32 +22,36 @@ function App() {
 
   return (
     <>
-      <header>
-        <h1>BoolFlix</h1>
-      </header>
-      <div className="container">
-        <div className="row">
-          <div className="col-12 col-md-8">
-            <input type="text" placeholder="Cerca" className='form-control' onChange={(e) => setInput(e.target.value)} />
-          </div>
-          <div className="col-12 col-md-4">
-            <button className='btn btn-secondary' onClick={handleClick}> Cerca </button>
+      <header className='py-3 mb-4'>
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-12 col-md-3 text-center text-md-start mb-2 mb-md-0">
+              <h1 className="text-white m-0">BoolFlix</h1>
+            </div>
+            <div className="col-12 col-md-7">
+              <input type="text" placeholder="Titolo" className='form-control' onChange={(e) => setInput(e.target.value)} />
+            </div>
+            <div className="col-12 col-md-2 mt-2 mt-md-0 text-center">
+              <button className='btn btn-secondary w-100' onClick={handleClick}> Cerca </button>
+            </div>
           </div>
         </div>
+      </header>
+      <div className="container">
         <div className="row mt-4">
           {film.map(movie => (
             <div key={movie.id} className="col-6 col-md-4 col-lg-3 mb-3">
               <div className="card">
                 <img
                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  className="card-img-top"
+                  className="card-img-top img-fluid"
                   alt={movie.title}
                 />
                 <div className="card-body">
                   <h5 className="card-title">{movie.title}</h5>
                   <p>{movie.original_title}</p>
-                  <p>{movie.original_language}</p>
-                  <p>{movie.vote_average}</p>
+                  <p><b>Language:</b> {movie.original_language}</p>
+                  <p><b>Vote:</b> {movie.vote_average}</p>
                 </div>
               </div>
             </div>
