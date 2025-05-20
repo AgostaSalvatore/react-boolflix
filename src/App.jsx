7d5986c8f582ea34ff269899b578d2b43f695b07
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-20 12:36:59 +0200

    feat: implement movie search functionality with TMDB API integration

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ const apiKey = '1aeffb0b3520785500a2dd89ea7e745f'
 function App() {
 
   const [input, setInput] = useState('');
+  const [film, setFilm] = useState([]);
 
   const handleClick = () => {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${input}`)
       .then((response) => {
-        console.log(response.data.results);
+        setFilm(response.data.results);
       })
       .catch((error) => {
         console.log(error);
       })
   }
+
+
 
   return (
     <>
@@ -31,6 +34,25 @@ function App() {
           <div className="col-12 col-md-4">
             <button className='btn btn-secondary' onClick={handleClick}> Cerca </button>
           </div>
+        </div>
+        <div className="row mt-4">
+          {film.map(movie => (
+            <div key={movie.id} className="col-6 col-md-4 col-lg-3 mb-3">
+              <div className="card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  className="card-img-top"
+                  alt={movie.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <p>{movie.original_title}</p>
+                  <p>{movie.original_language}</p>
+                  <p>{movie.vote_average}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
